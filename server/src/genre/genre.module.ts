0de1fde628e8f29f477cbf6abe8d439a44f6aa98
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-01-18 15:49:26 +0700

    basic description of the genre model

--- server/src/genre/genre.module.ts
+++ server/src/genre/genre.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
-import { GenreController } from './genre.controller';
-import { GenreService } from './genre.service';
+import { Module } from '@nestjs/common'
+import { TypegooseModule } from 'nestjs-typegoose'
+
+import { GenreController } from './genre.controller'
+import { GenreModel } from './genre.model'
+import { GenreService } from './genre.service'
 
 @Module({
   controllers: [GenreController],
-  providers: [GenreService]
+  providers: [GenreService],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: GenreModel,
+        schemaOptions: {
+          collection: 'Genre',
+        },
+      },
+    ]),
+  ],
 })
 export class GenreModule {}
