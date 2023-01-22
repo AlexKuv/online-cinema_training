ac3c897e57459d953b8072c0d11c64f32826a67b
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-01-22 21:24:26 +0700

    creating a model and DTO for the movie module

--- server/src/movie/dto/createMovie.dto.ts
+++ server/src/movie/dto/createMovie.dto.ts
@@ -1,1 +1,52 @@
-export class CreateMovieDto {}
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsObject,
+  IsString,
+} from 'class-validator'
+
+export class Parameters {
+  @IsNumber()
+  year: number
+
+  @IsNumber()
+  duration: number
+
+  @IsString()
+  county: string
+}
+
+export class CreateMovieDto {
+  @IsString()
+  poster: string
+
+  @IsString()
+  bigPoster: string
+
+  @IsString()
+  title: string
+
+  @IsString()
+  description: string
+
+  @IsString()
+  slug: string
+
+  @IsObject()
+  parameters?: Parameters
+
+  @IsString()
+  videoUrl: string
+
+  @IsArray()
+  @IsString({ each: true })
+  genres: string[]
+
+  @IsArray()
+  @IsString({ each: true })
+  actors: string[]
+
+  @IsBoolean()
+  isSendTelegram?: boolean
+}
--- server/src/movie/movie.module.ts
+++ server/src/movie/movie.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
-import { MovieController } from './movie.controller';
-import { MovieService } from './movie.service';
+import { Module } from '@nestjs/common'
+import { TypegooseModule } from 'nestjs-typegoose'
+
+import { MovieController } from './movie.controller'
+import { MovieModel } from './movie.model'
+import { MovieService } from './movie.service'
 
 @Module({
   controllers: [MovieController],
-  providers: [MovieService]
+  providers: [MovieService],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: MovieModel,
+        schemaOptions: {
+          collection: 'Movie',
+        },
+      },
+    ]),
+  ],
 })
 export class MovieModule {}
