01601ea9e3cd7cdcc0ed42a400b58fc1223a7a0c
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-01-25 18:23:07 +0700

    implementation of adding movies to favorites

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { InjectModel } from 'nestjs-typegoose'
+import { Types } from 'mongoose'
+import { genSalt, hash } from 'bcryptjs'
 
 import { UserModel } from './user.model'
 import { updateUserDto } from './dto/updateUser.dto'
-import { genSalt, hash } from 'bcryptjs'
 
 @Injectable()
 export class UserService {
@@ -63,5 +64,22 @@ export class UserService {
 
   async delete(id: string) {
     return this.UserModel.findByIdAndDelete(id).exec()
+  }
+
+  async toggleFavorite(movieId: Types.ObjectId, { _id, favorites }: UserModel) {
+    await this.UserModel.findByIdAndUpdate(_id, {
+      favorites: favorites.includes(movieId)
+        ? favorites.filter((id) => String(id) !== String(movieId))
+        : [...favorites, movieId],
+    })
+  }
+
+  async getFavoriteMovies(_id: Types.ObjectId) {
+    return (await this.UserModel.findById(_id, 'favorites'))
+      .populate({
+        path: 'favorites',
+        populate: { path: 'genres' },
+      })
+      .then((data) => data.favorites)
   }
 }
