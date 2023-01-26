b89885e068639793ddc6a7257a710a7c227f23f6
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-01-26 23:32:29 +0700

    model and dto implementation for the rating module

--- server/src/rating/rating.service.ts
+++ server/src/rating/rating.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+
+import { RatingModel } from './rating.model'
+import { MovieService } from 'src/movie/movie.service'
 
 @Injectable()
-export class RatingService {}
+export class RatingService {
+  constructor(
+    @InjectModel(RatingModel)
+    private readonly RatingModel: ModelType<RatingModel>,
+    private readonly movieService: MovieService
+  ) {}
+}
