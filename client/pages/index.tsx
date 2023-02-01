87cc012499f8b1cf8fdb5e397c4cba40ac6f9c7b
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-02-02 00:30:10 +0700

    installing and configuring tailwind

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,9 +14,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1 className="text-3xl font-bold">Hello!!</h1>
-      </div>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
@@ -29,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{" "}
+              By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -122,5 +119,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  );
+  )
 }
