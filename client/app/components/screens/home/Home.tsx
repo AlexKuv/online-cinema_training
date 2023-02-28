71779e97ac0be50a2f9222d919a182fada2b8a22
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-02-28 18:51:11 +0700

    adding meta information to a page

--- client/app/components/screens/home/Home.tsx
+++ client/app/components/screens/home/Home.tsx
@@ -1,13 +1,20 @@
-import Layout from '@/components/layout/Layout'
 import { FC } from 'react'
 
 import { IHome } from './home.interface'
+import Meta from '@/utils/meta/Meta'
+import Heading from '@/components/ui/heading/Heading'
 
 const Home: FC<IHome> = () => {
   return (
-    <Layout>
-      <div className="">Home</div>
-    </Layout>
+    <Meta
+      title="Watch movies online"
+      description="Watch MovieApp movies and TV shows online or stream right to your browser."
+    >
+      <Heading
+        title="Watch movies online"
+        className="text-gray-300 mb-8 text-xl"
+      />
+    </Meta>
   )
 }
 
