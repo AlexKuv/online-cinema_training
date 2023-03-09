562bb7979b26f9d7752c195fe7e1c60d5771bb59
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-03-10 02:33:43 +0700

    remove getGenresList

--- client/app/components/layout/Sidebar/MoviesContainer/MovieItem.tsx
+++ client/app/components/layout/Sidebar/MoviesContainer/MovieItem.tsx
@@ -1,11 +1,10 @@
-import { FC } from 'react'
+import { FC, Fragment } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import MaterialIcon from '@/components/ui/MaterialIcon'
 import { IMovie } from '@/shared/types/movie.types'
 import { getGenreUrl, getMovieUrl } from '@/config/url.config'
-import { getGenresListEach } from '@/utils/getGenresList'
 
 import styles from './MovieList.module.css'
 
@@ -28,9 +27,10 @@ const MovieItem: FC<{ movie: IMovie }> = ({ movie }) => {
         </Link>
         <div className={styles.genres}>
           {movie.genres.map((genre, index) => (
-            <Link key={genre._id} href={getGenreUrl(genre.slug)}>
-              {getGenresListEach(index, movie.genres.length, genre.name)}
-            </Link>
+            <Fragment key={genre._id}>
+              <Link href={getGenreUrl(genre.slug)}>{genre.name}</Link>
+              {index < movie.genres.length - 1 ? ', ' : ''}
+            </Fragment>
           ))}
         </div>
         <div className={styles.rating}>
